3b2dc78f7e48cb77f555ea63218188dc08a5d5aa
Atmajo <atmajoc@gmail.com>
2024-07-17 00:32:50 +0530

    first

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+
+const http = require("http");
+const path = require("path");
+
+const socket = require("socket.io");
+const server = http.createServer(app);
+const io = socket(server);
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  console.log("New connection");
+  socket.on("send-location", (coords) => {
+    console.log(coords);
+    io.emit("receive-location", { id: socket.id, ...coords });
+  });
+
+    socket.on("disconnect", () => {
+        io.emit("user-disconnected", socket.id);
+    });
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
